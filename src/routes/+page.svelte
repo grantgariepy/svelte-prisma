37e8c35da6a88256350eb9a0ff2c1c6c3be1660f
grantgariepy <grantgariepy@protonmail.com>
2023-01-23 21:09:46 -0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<div class="grid">
+	<div>
+		<h2>Articles:</h2>
+			<article>
+				<header>Example Article</header>
+				<p>
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores voluptates maiores itaque quibusdam ut alias adipisci ullam beatae numquam, quaerat animi qui nulla eligendi doloremque provident officia molestias nesciunt necessitatibus?
+				</p>
+					<button type="submit" class="outline secondary">Delete Article</button>
+				<a href="/" role="button" class="outline constrast" style="width: 100%;"
+					>Edit Article</a
+				>
+			</article>
+	</div>
+	<form action="" method="POST">
+		<h3>New Article</h3>
+		<label for="title"> Title </label>
+		<input type="text" id="title" name="title" />
+		<label for="title"> Title </label>
+		<textarea id="content" name="content" rows={5} />
+		<button type="submit">Add Article</button>
+	</form>
+</div>
